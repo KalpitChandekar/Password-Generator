156de157b811f57fd1bfe6f1391ddbb21d249f92
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-08-19 18:16:03 +0530

    created selectors

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const lowerSet = 'qwertyuiopasdfghjklzxcvbnm';
 const numberSet = '1234567890';
 const symbolSet = '!~@#$%^&()*+-/';
 
+const passBox = document.getElementById('pass-box');
+const totalChar = document.getElementById('total-char');
+const upperInput = document.getElementById('uper-case');
+const lowerInput = document.getElementById('lower-case');
+const numberInput = document.getElementById('numbers');
+const symbolInput = document.getElementById('symbols');
+
 const getRandomNumber = (dataset) => {
     return dataset[Math.floor(Math.random() * dataset.length)];
 }
