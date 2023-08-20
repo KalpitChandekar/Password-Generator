97896512199fbff30d518837e2638d02783d721b
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-08-20 21:08:21 +0530

    console the function

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const upperSet = 'QWERTYUIOPASDFGHJKLZXCVBNM';
-const lowerSet = 'qwertyuiopasdfghjklzxcvbnm';
-const numberSet = '1234567890';
-const symbolSet = '!~@#$%^&()*+-/';
+const upperSet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lowerSet = "qwertyuiopasdfghjklzxcvbnm";
+const numberSet = "1234567890";
+const symbolSet = "!~@#$%^&()*+-/";
 
-const passBox = document.getElementById('pass-box');
-const totalChar = document.getElementById('total-char');
-const upperInput = document.getElementById('uper-case');
-const lowerInput = document.getElementById('lower-case');
-const numberInput = document.getElementById('numbers');
-const symbolInput = document.getElementById('symbols');
+const passBox = document.getElementById("pass-box");
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("uper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
 
 const getRandomNumber = (dataset) => {
-    return dataset[Math.floor(Math.random() * dataset.length)];
-}
+  return dataset[Math.floor(Math.random() * dataset.length)];
+};
 
 const generatePassword = (password = "") => {
-if(upperInput.checked){
-    password=getRandomData(upperset);
-}
-
+  if (upperInput.checked) {
+    password += getRandomData(upperset);
+  }
+  console.log(password);
 };
